eed92c278e833beda029c0c12c4633ff98a5d5fa
Andrew Davisson <akdavisson4@gmail.com>
2019-06-01 00:45:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,6 +10,15 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
+  array = array.unshift(element);
+  return array;
+}
+
+function addElementToEndOfArray(array, element){
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
   array = array.push(element);
   return array;
 }
