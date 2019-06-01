d0739f387964f26669069dcac32a1b459c9fdb79
Andrew Davisson <akdavisson4@gmail.com>
2019-06-01 01:05:01 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -40,3 +40,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
   array.pop();
   return array;
 }
+
+function removeElementFromEndofArray(array){
+  return array.slice(0, array.length - 1);
+}
